9691ec943b65d1394e4c4e16dfeccd06df823568
rejoe2 <rejoe2@users.noreply.github.com>
2019-07-24 07:54:01 +0200

    Update 99_myUtils_ZWave.pm

--- 99_myUtils_ZWave.pm
+++ 99_myUtils_ZWave.pm
@@ -1,5 +1,5 @@
 ##############################################
-# $Id: myUtils_ZWave.pm 08-15 2019-06-19 06:13:42Z Beta-User $
+# $Id: myUtils_ZWave.pm 2019-07-24 Beta-User $
 #
 
 package main;
@@ -29,7 +29,7 @@ my $slatlevel= ReadingsNum($slatname,"state",0);
 #levelicon
 my $symbol_string = "fts_shutter_";
 my $command_string = "dim 99";
-$command_string = "off" if $dimlevel > 50;
+$command_string = "dim 0" if $dimlevel > 50;
 $symbol_string .= int ((109 - $dimlevel)/10)*10;
 $ret .= "<a href=\"/fhem?cmd.dummy=set $levelname $command_string&XHR=1\">" . FW_makeImage($symbol_string,"fts_shutter_10") . "</a> "; 
 
@@ -41,12 +41,43 @@ $symbol_string = "fts_blade_arc_close_";
 $command_string = "dim ";
 $slatlevel > 49 ? $symbol_string .= "00" : $slatlevel > 24 ? $symbol_string .= "50" : $slatlevel < 25 ? $symbol_string .= "100" : undef;
 $slatlevel > 49 ? $command_string = "off" : $slatlevel > 24 ? $command_string .= "50" : $slatlevel < 25 ? $command_string .= "25" : undef;
-$ret .= "<a href=\"/fhem?cmd.dummy=set $slatname $command_string&XHR=1\">" . FW_makeImage($symbol_string,"fts_blade_arc_close_100") . "</a> $slatlevel \%"; 
+$ret .= "<a href=\"/fhem?cmd.dummy=set $slatname $command_string&XHR=1\">" . FW_makeImage($symbol_string,"fts_blade_arc_close_100") . " $slatlevel \%</a>"; 
 
 return "<div><p style=\"text-align:right\">$ret</p></div>"
 ;
 }
 
+
+sub checkSlaveMovement($$) {
+my ($master,$event) = @_;
+return undef unless (ReadingsAge("Schalter_WZ1_Btn_03","triggerTo_$master",1000) < 2 or 
+                     ReadingsAge("Schalter_WZ1_Btn_04","triggerTo_$master",1000) < 2 or
+					 ReadingsAge("Schalter_WZ1_Btn_05","triggerTo_$master",1000) < 2 or 
+					 ReadingsAge("Schalter_WZ1_Btn_06","triggerTo_$master",1000) < 2);
+if ($event =~ /stop/) { 						  
+  CommandSet(undef,"Jalousie_WZ stop") 
+} elsif ($event =~ /up/) {
+  CommandSet(undef,"Jalousie_WZ dim 99") 
+} else {
+  CommandSet(undef,"Jalousie_WZ dim 0") 
+}
+
+}
+
+sub checkButtonMovement($$) {
+my ($master,$event) = @_;
+if ($master =~ /Schalter_WZ1_Btn_03/ && ReadingsAge($master,"triggerTo_Rolladen_WZ_SSO",1000) > 10) {
+  CommandSet(undef,"Jalousie_WZ dim 0"); 
+} elsif ($master =~ /Schalter_WZ1_Btn_05/ && ReadingsAge($master,"triggerTo_Jalousie_Rechts",1000) > 10) {
+  CommandSet(undef,"Jalousie_WZ dim 0"); 
+} elsif ($master =~ /Schalter_WZ1_Btn_06/ && ReadingsAge($master,"triggerTo_Jalousie_Rechts",1000) > 10) {
+  CommandSet(undef,"Jalousie_WZ dim 99"); 
+} elsif ($master =~ /Schalter_WZ1_Btn_04/ && ReadingsAge($master,"triggerTo_Rolladen_WZ_SSO",1000) > 10) {
+  CommandSet(undef,"Jalousie_WZ dim 99"); 
+} elsif ($master =~ /Schalter_WZ1_Btn_0[56]/ && ReadingsVal("Jalousie_Rechts","motor","stop") =~ /stop/ or $master =~ /Schalter_WZ1_Btn_0[34]/ && ReadingsVal("Rolladen_WZ_SSO","motor","stop") =~ /stop/) {
+  CommandSet(undef,"Jalousie_WZ stop") ;
+}
+}
 
 1;
 
