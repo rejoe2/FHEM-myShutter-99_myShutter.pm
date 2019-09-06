f8e5a812b486077bbdb98fd8098c7865db879df7
rejoe2 <rejoe2@users.noreply.github.com>
2019-09-06 17:05:17 +0200

    Create 99_myUtilsSnipplets.pm

---
+++ 99_myUtilsSnipplets.pm
@@ -0,0 +1,2 @@
+#Beispiel für Hash-Nutzung in userReadings:
+# { my %rets = ("Error"  => "Error","0" => "ist aus","1" => "Standby","2" => "initialisiert","3" => "wird geladen","4" => "wird entladen","5" => "Fehler","6"  => "Leerlauf","7"  => "Leerlauf",);; my $val = ReadingsVal("SolarEdge", "Batt_State", "Error");; my $val = ReadingsVal("SolarEdge", "Batt_State", "Error");; $rets{$val};;}
