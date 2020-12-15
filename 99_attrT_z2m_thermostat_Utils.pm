69c4fa2ac9cf1060bf84525bdfdd61ec0307e2fe
rejoe2 <rejoe2@users.noreply.github.com>
2020-12-15 12:07:33 +0100

    Add files via upload

--- 99_attrT_z2m_thermostat_Utils.pm
+++ 99_attrT_z2m_thermostat_Utils.pm
@@ -59,6 +59,7 @@ sub z2t_send_weekprofile {
   my $wp_name    = shift;
   my $wp_profile = shift // return;
   my $topic      = shift // AttrVal($name,'devicetopic','') . '/set';
+  my $model      = shift // ReadingsVal($name,'week','5+2');
   
   my $hash = $defs{$name};
   $topic   .= ' ';
@@ -70,40 +71,41 @@ sub z2t_send_weekprofile {
   }
   
   my @D = ("Sat","Sun","Mon","Tue","Wed","Thu","Fri");
-  my $payload = '{"holidays":[';
+  my $payload;
+  my @days = (0..6);
   my $text = decode_json($wp_profile_data);
-
-
-
-  for my $i (0..6) {
+  
+  if ( $model eq '5+2' || $model eq '6+1') {
+    @days = (1,2);
+	$payload = '{"holidays":[';
+  } elsif ($model eq '7') {
+    @days = (2);
+	$payload = '{"workdays":[';
+  }
+  
+  for my $i (@days) {
       $payload.='{';
+      
       for my $j (0..7) {
         if (defined $text->{$D[$i]}{'time'}[$j]) {
           my $time = $text->{$D[$i]}{'time'}[$j-1] // "00:00";
           my ($hour,$minute) = split m{:}xms, $time;
-          $payload .= '"hour":' . z2t_hour($hour,$i) .',"minute":'. abs($minute) .',"temperature":'.$text->{$D[$i]}{'temp'}[$j];
-          $payload .= ',' if defined $text->{$D[$i]}{'time'}[$j+1];
+          $hour = 0 if $hour == 24;
+          $payload .= '"hour":' . abs($hour) .',"minute":'. abs($minute) .',"temperature":'.$text->{$D[$i]}{'temp'}[$j];
+          $payload .= '},{' if defined $text->{$D[$i]}{'time'}[$j+1];
         }
       }
       $payload .='}';
-	  $payload .=','if $i == 0 || $i > 1 && $i < 6;
-	  $payload .='],"workdays":[' if $i == 1;
-	  $payload .=']' if $i == 6;
+	  if ($model eq '5+2' || $model eq '6+1') {
+        $payload .='},'if $i == 0 || $i > 1 && $i != $days[-1];
+	    $payload .='],"workdays":[' if $i == 1;
+      }
   }
-  $payload .='}';
+  $payload .=']}';
   
   return "$topic $payload";
 }
   
-sub z2t_hour {
-  my $hour = shift;
-  my $day  = shift // return;
-  
-  $day  = $day < 2 ? $day + 5 : $day - 2; #start week with monday
-  ;$hour = 0 if $hour == 24;
-  $hour = $day*24 + $hour;
-  return $hour;
-}
 
 1;
 
